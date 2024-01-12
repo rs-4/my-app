034d46bc96532ed992f9883bb9824d3996d91168
Vincent Zerbib <contact@zelabstudio.com>
2024-01-12 12:54:14 +0100

    save work

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -6,26 +6,74 @@ const { getArticles, getArticle } = require('../controllers/article.controller')
 
 /**
  * @openapi
+ *  
  * /api/articles:
  *   get:
- *     description: Get all articles 
+ *     summary: Get all articles
+ *     description: Retrieve a list of all articles available in the system.
+ *     tags:
+ *       - Articles
  *     responses:
  *       200:
- *         description: Returns an array of article.
+ *         description: A list of articles.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 properties:
+ *                 title: 
+ *                  type: string
+ *                 description:
+ *                  type: string        
+ *                 date:
+ *                  type: string
+ *       400:
+ *         description: Bad request. Indicates that the request was invalid.
+ *       500:
+ *         description: Internal server error. Indicates a problem on the server side.
+ * 
  */
 
 router.get('/', getArticles);
 
-
 /**
  * @openapi
- * /api/article:
+ *  
+ * /api/articles/{id}:
  *   get:
- *     description: Get one article 
+ *     summary: Get one article
+ *     description: Retrieve one article from a foreign key
+ *     tags:
+ *       - Articles
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        type: integer
+ *        description: The id of the article to retrieve
  *     responses:
  *       200:
- *         description: Returns an article.
+ *         description: One article.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               items:
+ *                 properties:
+ *                 title: 
+ *                  type: string
+ *                 description:
+ *                  type: string        
+ *                 date:
+ *                  type: string
+ *       400:
+ *         description: Bad request. Indicates that the request was invalid.
+ *       500:
+ *         description: Internal server error. Indicates a problem on the server side.
+ * 
  */
+
 router.get('/:id', getArticle);
 
 module.exports = router;
